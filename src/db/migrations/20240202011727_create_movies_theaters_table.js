664042ee2dd73df3960a1f77995f31f6969c1a72
Rume <Sprtrks@gmail.com>
2024-04-30 16:32:40 -0400

    tablesIII

--- src/db/migrations/20240202011727_create_movies_theaters_table.js
+++ src/db/migrations/20240202011727_create_movies_theaters_table.js
@@ -3,12 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-    return knex.schema.createTable('movies_theaters', function(table) {
-      table.integer('movie_id').references('movies.movie_id');
-      table.integer('theater_id').references('theaters.theater_id');
-      table.boolean('is_showing').defaultTo(false); // Default value for boolean column
-    });
-  };
+  return knex.schema.createTable("movies_theaters", (table) => {
+        table.boolean("is_showing").defaultTo(false);
+        table.timestamps(true, true);
+    
+        table.integer("movie_id").unsigned().notNullable();
+        table.foreign("movie_id").references("movie_id").inTable("movies");
+    
+        table.integer("theater_id").unsigned().notNullable();
+        table.foreign("theater_id").references("theater_id").inTable("theaters");
+      });
+};
 
 /**
  * @param { import("knex").Knex } knex
