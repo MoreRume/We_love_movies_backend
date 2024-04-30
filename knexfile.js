a5ef3a87221be553651359130da159898e46df49
Rume <Sprtrks@gmail.com>
2024-04-30 12:52:57 -0400

    database connection

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,61 @@
+require('dotenv').config();
 const path = require("path");
 
-require("dotenv").config();
-
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://enpyamez:DpmriXDJhvgiQNOd6fQp5wVWOgcG_CTk@fanny.db.elephantsql.com/enpyamez",
+  DATABASE_URL_DEVELOPMENT = "postgres://enpyamez:DpmriXDJhvgiQNOd6fQp5wVWOgcG_CTk@fanny.db.elephantsql.com/enpyamez",
+  DATABASE_URL_TEST = "postgres://enpyamez:DpmriXDJhvgiQNOd6fQp5wVWOgcG_CTk@fanny.db.elephantsql.com/enpyamez",
+  DATABASE_URL_PREVIEW = "postgres://enpyamez:DpmriXDJhvgiQNOd6fQp5wVWOgcG_CTk@fanny.db.elephantsql.com/enpyamez",
+  DEBUG,
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    pool: { min: 1, max: 5 },
+    connection: DATABASE_URL_DEVELOPMENT,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
+    debug: !!DEBUG,
   },
-
+  test: {
+    client: "postgresql",
+    pool: { min: 1, max: 5 },
+    connection: DATABASE_URL_TEST,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: !!DEBUG,
+  },
+  preview: {
+    client: "postgresql",
+    pool: { min: 1, max: 5 },
+    connection: DATABASE_URL_PREVIEW,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: !!DEBUG,
+  },
   production: {
     client: "postgresql",
+    pool: { min: 1, max: 5 },
     connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-  },
-
-  test: {
-    client: "sqlite3",
-    connection: {
-      filename: ":memory:",
-    },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
+    debug: !!DEBUG,
   },
 };
